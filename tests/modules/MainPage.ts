ede1fb5ec103ebf49005606813f5d23af6a8d3f4
vidombax <kirilka360@gmail.com>
2025-11-25 22:48:43 +0500

    добавил автотесты на блоки главной страницы

--- tests/modules/MainPage.ts
+++ tests/modules/MainPage.ts
@@ -1,4 +1,4 @@
-import test, {Locator, Page} from "@playwright/test";
+import test, {Locator, Page, expect} from "@playwright/test";
 
 interface Elements {
   search: {
@@ -7,9 +7,19 @@ interface Elements {
   }
 }
 
+interface Blocks {
+  locator: (page: Page) => Locator;
+  text: string;
+  attributes?: Array<{
+    type: string;
+    value: string;
+  }>;
+}
+
 export class MainPage {
     readonly page: Page;
     readonly elements: Elements;
+    readonly blocks: Blocks[];
 
     constructor(page: Page) {
         this.page = page;
@@ -19,18 +29,124 @@ export class MainPage {
             value: 'Дандадан'
           },
         }
+        this.blocks = [
+          {
+            locator: (page: Page): Locator => page.locator('a').filter({ hasText: 'Сейчас на экранах' }),
+            text: 'Сейчас на экранах',
+            attributes: [
+              {
+                type: 'href', 
+                value: 'https://shikimori.one/animes/status/ongoing'
+              }
+            ]
+          },
+          {
+            locator: (page: Page): Locator => page.locator('//*[@id="dashboards_show"]/section/div/div/div[3]/div/div[3]/div[1]/div[1]/div/a'),
+            text: 'Аниме',
+            attributes: [
+              {
+                type: 'href', 
+                value: 'https://shikimori.one/animes'
+              }
+            ]
+          },
+          {
+            locator: (page: Page): Locator => page.locator('//*[@id="dashboards_show"]/section/div/div/div[3]/div/div[3]/div[1]/div[2]/div/a'),
+            text: 'Манга',
+            attributes: [
+              {
+                type: 'href', 
+                value: 'https://shikimori.one/mangas'
+              }
+            ]
+          },
+          {
+            locator: (page: Page): Locator => page.locator('//*[@id="dashboards_show"]/section/div/div/div[4]/div[2]'),
+            text: 'Новости',
+          },
+          {
+            locator: (page: Page): Locator => page.locator('//*[@id="dashboards_show"]/section/div/div/div[5]/div/div[1]/div[1]/div/a[1]'),
+            text: 'коллекции',
+            attributes: [
+              {
+                type: 'href', 
+                value: 'https://shikimori.one/collections'
+              }
+            ]
+          },
+          {
+            locator: (page: Page): Locator => page.locator('//*[@id="dashboards_show"]/section/div/div/div[5]/div/div[1]/div[1]/div/a[2]'),
+            text: 'рецензии',
+            attributes: [
+              {
+                type: 'href', 
+                value: 'https://shikimori.one/forum/critiques'
+              }
+            ]
+          },
+          {
+            locator: (page: Page): Locator => page.locator('//*[@id="dashboards_show"]/section/div/div/div[5]/div/div[1]/div[1]/div/a[3]'),
+            text: 'статьи',
+            attributes: [
+              {
+                type: 'href', 
+                value: 'https://shikimori.one/articles'
+              }
+            ]
+          },
+          {
+            locator: (page: Page): Locator => page.getByText('Темы дня').nth(1),
+            text: 'Темы дня',
+          },
+          {
+            locator: (page: Page): Locator => page.locator('a[href="https://shikimori.one/forum/updates"]'),
+            text: 'Обновления аниме',
+            attributes: [
+              {
+                type: 'href', 
+                value: 'https://shikimori.one/forum/updates'
+              }
+            ]
+          },
+          {
+            locator: (page: Page): Locator => page.getByText('Ещё новости'),
+            text: 'Ещё новости',
+          },
+        ]
     }
 
     async openMainPage() {
         await this.page.goto('https://shikimori.one');
     }
 
-    async checkNamesOnBlocks() {
+    async checkBlocksVisibility() {
+      for (const {locator, text} of this.blocks) {
+            await test.step(`Проверка отображения элемента ${text}`, async () => {
+                await expect(locator(this.page)).toBeVisible();
+            });
+        };
+    }
 
+    async checkNamesOnBlocks() {
+      for (const {locator, text} of this.blocks) {
+            if (text) {
+                await test.step(`Проверка названия элемента ${text}`, async () => {
+                    await expect(locator(this.page)).toContainText(text);
+                });
+            }
+        }
     }
 
     async checkHrefAttributeOnLinks() {
-      
+      for (const element of this.blocks) {
+              if (element.attributes) {
+                for (const attribute of element.attributes) {
+                  await test.step(`Проверка атрибута ${attribute.type} на элементе ${element.text}`, async () => {
+                    await expect(element.locator(this.page)).toHaveAttribute(attribute.type, attribute.value);
+                  });
+                }
+              }
+        }
     }
 
     async searchAnime() {
--- tests/pages/mainPage.spec.ts
+++ tests/pages/mainPage.spec.ts
@@ -1,7 +1,20 @@
 import { test } from '../fixtures/mainPage';
 
 test.describe('Тесты главной страницы', () => {
+  test('Проверка отображение блоков', async ({ mainPage }) => {
+    await mainPage.checkBlocksVisibility();
+  });
+
+  test('Проверка отображение текста на блоках', async ({ mainPage }) => {
+    await mainPage.checkNamesOnBlocks();
+  });
+
+  test('Проверка href атрибута на блоках', async ({ mainPage }) => {
+    await mainPage.checkHrefAttributeOnLinks();
+  });
+
   test('Проверка поиска', async ({ mainPage }) => {
     await mainPage.searchAnime();
   });
+
 });
